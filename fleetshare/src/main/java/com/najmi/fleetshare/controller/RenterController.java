fffe5d4b963355f2bbfc66bf9aad93d7e99f76f4
Naim Najmi <naim.hazre@gmail.com>
2025-11-27 11:56:37 +0800

    Implement database to vehicle browsing

--- fleetshare/src/main/java/com/najmi/fleetshare/controller/RenterController.java
+++ fleetshare/src/main/java/com/najmi/fleetshare/controller/RenterController.java
@@ -1,16 +1,25 @@
 package com.najmi.fleetshare.controller;
 
 import com.najmi.fleetshare.dto.SessionUser;
+import com.najmi.fleetshare.dto.VehicleDTO;
+import com.najmi.fleetshare.service.VehicleManagementService;
 import com.najmi.fleetshare.util.SessionHelper;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping("/renter")
 public class RenterController {
+
+    @Autowired
+    private VehicleManagementService vehicleManagementService;
 
     @GetMapping("/vehicles")
     public String browseVehicles(HttpSession session, Model model) {
@@ -21,7 +30,14 @@ public class RenterController {
                 model.addAttribute("renterName", user.getRenterDetails().getFullName());
             }
         }
-        // TODO: Add vehicle list from service
+
+        // Fetch all vehicles and filter to show only available ones
+        List<VehicleDTO> allVehicles = vehicleManagementService.getAllVehicles();
+        List<VehicleDTO> availableVehicles = allVehicles.stream()
+                .filter(vehicle -> "AVAILABLE".equals(vehicle.getStatus()))
+                .collect(Collectors.toList());
+
+        model.addAttribute("vehicles", availableVehicles);
         return "renter/browse-vehicles";
     }
 
