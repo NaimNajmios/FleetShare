ebe25476fa9d0bbad7ce582b0c8a6e0e79b1f832
Naim Najmi <naim.hazre@gmail.com>
2026-05-25 11:59:30 +0800

    feat: implement design system CSS variables and off-canvas navigation utility

--- fleetshare/src/main/resources/static/assets/js/off-canvas.js
+++ fleetshare/src/main/resources/static/assets/js/off-canvas.js
@@ -1,20 +1,8 @@
 (function($) {
   'use strict';
   $(function() {
-    // Restore state from localStorage on page load
-    if (localStorage.getItem('mobileMenuState') === 'open') {
-        $('.sidebar-offcanvas').addClass('active');
-    }
-
     $('[data-toggle="offcanvas"]').on("click", function() {
       $('.sidebar-offcanvas').toggleClass('active');
-      
-      // Save the new state to localStorage
-      if ($('.sidebar-offcanvas').hasClass('active')) {
-          localStorage.setItem('mobileMenuState', 'open');
-      } else {
-          localStorage.setItem('mobileMenuState', 'closed');
-      }
     });
   });
 })(jQuery);
