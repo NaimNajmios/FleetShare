2205f06ef814d5950b7ba6f81cacb77ef3727e4a
Naim Najmi <naim.hazre@gmail.com>
2026-06-25 15:52:01 +0800

    feat: implement ReportService for multi-category data aggregation and report generation

--- fleetshare/src/main/java/com/najmi/fleetshare/service/ReportService.java
+++ fleetshare/src/main/java/com/najmi/fleetshare/service/ReportService.java
@@ -5,7 +5,6 @@ import com.najmi.fleetshare.entity.Vehicle;
 import com.najmi.fleetshare.repository.*;
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
